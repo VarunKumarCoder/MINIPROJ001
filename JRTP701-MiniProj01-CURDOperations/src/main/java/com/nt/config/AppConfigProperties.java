b279be7052b9b72a971a39ab1dd4fc1bd97fd3b8
VarunKumarCoder <medavarunkumar@gmail.com>
2025-02-11 17:46:55 +0530

    Developed configuration class with messages property linking to the property of messages in Yaml file

--- JRTP701-MiniProj01-CURDOperations/src/main/java/com/nt/config/AppConfigProperties.java
+++ JRTP701-MiniProj01-CURDOperations/src/main/java/com/nt/config/AppConfigProperties.java
@@ -1,5 +1,17 @@
 package com.nt.config;
 
-public class AppConfigProperties {
+import java.util.Map;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@Data
+@ConfigurationProperties(prefix="plan.module")
+@EnableConfigurationProperties
+public class AppConfigProperties {
+	private Map<String, String> messages;
 }
